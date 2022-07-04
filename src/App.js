212e81aa6de4235049da64238e36840bab57de82
SAURABH JAISWAL <hashproxyindia@gmail.com>
2022-07-04 20:10:24 +0530

    done with patterns and some UI part

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer/Footer';
 import GameBoard from './components/GameBoard/GameBoard';
+import PlayState from './components/PlayState/PlayState';
 
 
-function App(props) {
+function App() {
 
 
   return (
     <div className="App">
-        <GameBoard />
+      <div className='MainBoard'>
+       <h1 className='page-title'>TIC TAC TOE</h1>
+        <Routes >
+          <Route path='GameBoard' element={ <GameBoard />} />
+          <Route path='/' element={ <PlayState />}/>
+        </Routes>
+        </div>        
+        <Footer title={'Created By Saurabh Jaiswal'} />
     </div>
   );
 }
